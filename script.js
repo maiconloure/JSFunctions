592b61c480b9beb042f2514f1e649c5d046c7ebc
Maicon Lourenço <maiconloure@gmail.com>
2020-07-05 23:27:12 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+const newForEach = () => {
+}
+
+
+const newMap = () => {
+}
+
+
+const newSome = () => {
+}
+
+
+const newFind = () => {
+}
+
+
+const newFindIndex = () => {
+}
+
+
+const newEvery = () => {
+}
+
+
+const newFilter = () => {
+}
+
+
+const newConcat = () => {
+}
+
+
+const newIncludes = () => {
+}
+
+
+const newIndexOf = () => {
+}
+
+
+const newJoin = () => {
+}
+
+
+const newSlice = () => {
+}
+
+
+const newFlat = () => {
+}
+
+
+const newFlatMap = () => {
+}
+
+
+const newArrayOf = () => {
+}
